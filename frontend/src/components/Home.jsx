23dcb57db7993d51226ec1378f7c079d9b875cd1
toheedakhtar <toheedak333@gmail.com>
2024-10-24 13:00:10 +0530

    starting up

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,12 +7,12 @@ import MySongs from './MySongs'
 const Home = () => {
   return (
     <>
-    <div className="p-5 bg-black text-white"><Navbar /></div>
-    <div className="h-screen flex text-white">
-      <div className="bg-black w-1/2 p-2"> <MySongs/> </div>
-      <div className="bg-black w-1/2 p-2"> <Recommendation /> </div>
-      <div></div>
-    </div>
+      <div className="p-5 bg-black text-white"><Navbar /></div>
+      <div className="h-screen flex text-white">
+        {/* <div className="bg-black w-1/2 p-2"> <MySongs/> </div> */}
+        <div className="bg-black w-full p-2"> <Recommendation /> </div>
+        <div></div>
+      </div>
 
     </>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = () => {
   return (
     <div className='flex flex-row justify-between'>
       <div>
-        <img src="src/assets/logo.png" alt='beatSync' className='w-12 h-15 rounded-sm'/>
+        <img src="src/assets/logo.png" alt='beatSync' className='w-12 h-15 rounded-sm' />
       </div>
       <div className='font-bold text-2xl font-serif text-green-500'>beatSync</div>
-      <div className='flex space-x-3'>
-        <div>Login</div>
-        <div>Signup</div>
-      </div>
+      {/* <div className='flex space-x-3'> */}
+      {/* <div>Login</div>
+        <div>Signup</div> */}
+      {/* </div> */}
     </div>
-    
+
   )
 }
 
