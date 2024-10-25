f705523b28e7836db164d746115ba4aae5b85e51
toheedakhtar <toheedak333@gmail.com>
2024-10-25 18:41:14 +0530

    frontend : rendered dummy image + song name

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -8,10 +8,9 @@ import Navbar from './Navbar'
 const Home = () => {
   return (
     <>
-      <div className="p-5 bg-black text-white"><Navbar /></div>
-      <div className="h-full flex text-white">
-
-        <div className="bg-black w-full h-screen p-2"> <Recommendation /> </div>
+      <div className="p-2 bg-black text-white"><Navbar /></div>
+      <div className="bg-black h-screen text-white">
+        <div className="bg-black p-2"> <Recommendation /> </div>
 
       </div>
 
--- frontend/src/components/Recommendation.jsx
+++ frontend/src/components/Recommendation.jsx
@@ -25,9 +25,8 @@ const Recommendation = () => {
     e.preventDefault()
     try {
       const res = await axios.post('http://127.0.0.1:8000/api/song', { value })
-      setRecommend(res.data.slice(1))
-      // console.log(res.data)
-      // setRecommend(res.data)
+      console.log(res.data)
+      setRecommend(res.data)
       // console.log('Successfully Send',)
     }
     catch (e) {
@@ -38,23 +37,32 @@ const Recommendation = () => {
 
 
   return (
-    <div className='flex flex-col space-y-2  bg-[#121212] w-full h-full p-2'>
+    <div className='flex flex-col space-y-2 h-full bg-[#121212] p-8 mx-[5%] border-none rounded-lg'>
       <form action='POST'>
-        <div className='text-white font-semibold text-xl  p-2 rounded-md'><h1>Choose a song you vibe with...</h1></div>
+        <div className='text-white font-semibold text-xl p-2 rounded-md'><h1>Choose a song you vibe with...</h1></div>
+
         <div className='py-4'>
 
           <Select options={options} className='text-black' onChange={handleSelect} />
 
         </div>
+
         <div>
-          <button className='bg-green-400 text-black font-semibold p-3 rounded-md outline-none' onClick={submit}>Recommend</button>
+
+          <button className='bg-black text-green-400 font-semibold p-2 rounded-md outline hover:bg-green-400 hover:border-black border-3 hover:text-black' onClick={submit}>Recommend</button>
+
         </div>
 
       </form>
 
-      <div className='m-2 p-2'>
-        {recommends.map((song, index) => (
-          <li key={index}>{song}</li>
+      <div className='m-2 p-2 h-full text-wrap grid lg:grid-cols-4 md:grid-cols-2'>
+        {Object.entries(recommends).map(([song, url], index) => (
+          <div key={index} className='flex flex-col p-8 m-5'>
+            <img src={url} alt='image' width={250} height={250} className='border-none rounded-md' />
+            <span className='p-2 sm:text-sm md:text-sm lg:text-lg' >{song}</span>
+          </div>
+
+
         ))}
       </div>
     </div>
